d33e5bb7758f80ee53339070e74ea19c36362963
BrandeYarborough <brandeyarborough@gmail.com>
2023-01-29 13:26:53 -0500

    adds event listener for attack buttons

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+import { Battle } from "./battle";
 import { Character } from "./character";
+const selectPlayer = document.querySelector(".dropdown-menu");
+selectPlayer.addEventListener("click", function (event) {
+  const result = document.querySelector(".result");
+  const namePrompt = prompt("Enter your name");
+  const value = event.target.getAttribute("value");
+  console.log(value);
+  const name = document.querySelector(".name");
+  name.innerHTML = `${namePrompt}`;
+  result.innerHTML = `${value}`;
+  console.log(name);
+});
+
+let attack = document("attack buttons");
+attack.addEventListener("click", () => {
+  //calls turn function from new Battle object w/value of selected attack
+  Battle.turn(e.target.value);
+});
